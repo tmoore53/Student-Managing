a19c364c01c57c7b8af5cc75b152855cdc8db650
Tyler Moore <tyler.moore53@outlook.com>
2020-02-28 09:54:50 -0800

    Commit Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,52 +18,77 @@ namespace ConsoleApp1
             //var studentGrades = new int[classCount];
 
 
-            var student = new List<Student>();
+            var studentName = new List<Student>();
 
+            var adding  = true;
 
-            var answer = true;
-
-            while(answer)
+            while (adding)
             {
-                var students = new Student();
+                Student students = new Student();
                 Console.WriteLine("What is the student's name?");
-                students.Name  = Console.ReadLine();
+                students.Name = Console.ReadLine();
+
                 Console.WriteLine("What is the student's grade");
-                students.Grade = int.Parse(Console.ReadLine());
+                students.Grade = (int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("What is the student's favorite number");
+                students.FavNum = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("What is the student's birthday");
+                students.Birthday = Console.ReadLine();
+
+                Console.WriteLine("What is the student's phone number");
+                students.Phone = Double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Add another student? y/n");
 
-                student.Add(students);
-                
-                if (Console.ReadLine() != "y")
+                studentName.Add(students);
+                Student.Count++;//inciments the count by 1 from the student class
+                Console.WriteLine("Student Count: {0}", Student.Count);//displays the static count
+
+
+                string sws = Console.ReadLine();
+
+                if ( sws.ToLower() != "y")
                 {
-                    answer = false;
+                    adding = false;
                 }
-            
-               
             }
 
-
-            foreach (var students in student ) 
+            foreach (var students in studentName)
             {
-                Console.WriteLine("Name: {0}, Grade: {1}", students.Name, students.Grade);
-                    
+                Console.WriteLine("Name: {0}, Grade: {1}, Favorite Number: {2}, Birthday: {3}", students.Name, students.Grade, students.FavNum, students.Birthday);
             }
-            
-            //for (int i = 0; i < student.Count; i++)
-            //{
-            //    Console.WriteLine("Name: {0}, Grade: {1}",student[i], student[i]);
-            //}
 
         }
         class Student
         {
+            static public int Count = 0 ;//initializing to 0 isn't necessary, but helps show that you want to start at 0.
+
             public string Name;
             public string Birthday;
-            public int Age;
-            public int Phone;
+            public int FavNum;
+            private double phone;
             public int Grade;
 
+            public double Phone
+            {
+                set { phone = value; Console.WriteLine("You entered the phone number: {0}",phone); }
+            }
+
+
+           // public void SetPhone(double number)
+            //{
+            //    phone = number;
+           // }
+
+
+
         }
+
+
     }
 }
+   
+
+
